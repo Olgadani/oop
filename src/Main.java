63d46ffa0b1b1a2f1e84cec9545fa9b702b92e39
Olgadani <olga.metizzz@yandex.ru>
2022-10-25 23:05:11 +0400

    hw

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import Transport.Car;
 public class Main {
     public static void main(String[] args) {
         Person maksim = new Person("Maksim", 35, "Minsk", "бренд - менеджер");
@@ -12,5 +13,15 @@ public class Main {
         vladimir.printGreeting();
         Flower rose = new Flower("Роза обыкновенная", "Голландия", 35.59,0 );
         System.out.println(rose);
+        Car lada = new Car("Lada", "Granta", 1.7, "", 2015, "Россия");
+        Car audi = new Car("Audi", "A8 50 L TDI quattro", 3.0, "черный", 2020, "Германия");
+        Car bmw = new Car("BMW", "Z8", 3.0, "черный", 2021, "Германия");
+        Car kia = new Car("Kia", "Sportage 4-го поколения", 2.4, "красный", 2018, "Южная Корея");
+        Car hyundai = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея");
+        lada.printCar();
+        audi.printCar();
+        bmw.printCar();
+        kia.printCar();
+        hyundai.printCar();
     }
 }
